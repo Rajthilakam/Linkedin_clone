774d5c924fb6c99289bd7a571ab15a2232f03111
rajthilakam <rajthilakam@gmail.com>
2021-07-19 17:54:28 -0700

    Routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = 
+{
+    mongoUrl:'mongodb+srv://Mohita22:Mohita22@cluster0.hzcup.mongodb.net/socialmediadb?retryWrites=true&w=majority' 
+}
--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const  mongoose = require('mongoose')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile') 
 const app = express()
+const keys = require('./config/keys')
+
+
+
+//Db Config
+const db = keys.mongoUrl
+mongoose
+    .connect(db)
+    .then(() => console.log("Connected to db"))
+    .catch(err => console.log(err))
+
 
 //Routes
-app.get('/', (req,res) => res.send('Hello World! Hello'))
+app.get('/', (req,res) => res.send('Hello World!'))
 
+
+//Middleware functions
+app.use('/api/users',users)
+app.use('/api/posts',posts)
+app.use('/api/profile',profile)
+
+
+//Starting a server
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+
+//
